ee5165bcf07f59b2f4180e73ff2cfc99a141198e
Shayan <162302193+Shayan123458@users.noreply.github.com>
2024-05-18 23:56:58 +0500

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
-var personName = "Quaid-e-Azam";
-var famousQuote = "There is no power on earth that can undo Pakistan";
-console.log("".concat(personName, " once said, ").concat(famousQuote));
+console.log("Quaid-e-Azam once said, There is no power on earth that can undo Pakistan")
