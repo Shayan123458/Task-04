864677337f4d0bdb3206ee9eb9c10ba32b6cc027
unknown <Wardan Ahmed>
2024-05-18 23:13:32 +0500

    push my task 04

---
+++ app.js
@@ -0,0 +1,3 @@
+var personName = "Quaid-e-Azam";
+var famousQuote = "There is no power on earth that can undo Pakistan";
+console.log("".concat(personName, " once said, ").concat(famousQuote));
---
+++ app.ts
@@ -0,0 +1,3 @@
+let personName = "Quaid-e-Azam";
+let famousQuote = "There is no power on earth that can undo Pakistan"
+console.log(`${personName} once said, ${famousQuote}`)
